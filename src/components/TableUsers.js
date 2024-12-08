656a7ff55ec1991fe4e0d99955b00d9c04e32803
Quang-Dai123 <tranquangdai2257@gmail.com>
2024-12-08 13:09:36 +0700

    add customize-axios.js

---
+++ src/components/TableUsers.js
@@ -0,0 +1,53 @@
+import Table from "react-bootstrap/Table";
+import { useEffect, useState } from "react";
+import {fetchAllUser} from "../services/userSevice"
+
+const TableUsers = (props) => {
+
+    const [listUsers,setListUsers] = useState([]);
+
+    useEffect(() => {
+        //call apis
+        getUsers();
+        
+    }, [])
+
+    const getUsers = async () => {
+        let res = await  fetchAllUser();
+        console.log(">>>check new res: ", res)
+        if(res && res.data) {
+            setListUsers(res.data)
+        }
+    }
+
+
+    return (
+        <>
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                {listUsers && listUsers.length >0 && 
+                listUsers.map((item, index) => {
+                    return (
+                        <tr key={`users-${index}`}>
+                            <td>{item.id}</td>
+                            <td>{item.email}</td>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </Table>
+        </>
+    );
+};
+
+export default TableUsers;
